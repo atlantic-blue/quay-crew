4c0473c336b2a70addeeb68c7a387284ffc3fa79
juliantellez <4896851+juliantellez@users.noreply.github.com>
2026-08-30 02:23:26 +0000

    446: test: a scenario reads a job asking a person and carrying on with the answer

--- features/suite_test.go
+++ features/suite_test.go
@@ -687,6 +687,7 @@ func initializeScenario(sc *godog.ScenarioContext) {
 	initializeLifecycleSteps(sc)
 	initializeJobEventsSteps(sc)
 	initializeJobLeaseSteps(sc)
+	initializeAskingSteps(sc)
 	initializeCapabilitySteps(sc)
 	initializeTasksViewSteps(sc)
 	initializeAttachSteps(sc)
